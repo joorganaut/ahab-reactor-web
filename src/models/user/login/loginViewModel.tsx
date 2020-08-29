6cef9d6cb8f6541d9cf3712bac045b116c1d0520
Joorgi Spectra <sazeespectra@gmail.com>
2020-08-29 02:59:05 -0400

    Exchange system

--- src/models/user/login/loginViewModel.tsx
+++ src/models/user/login/loginViewModel.tsx
@@ -22,27 +22,28 @@ export default class LoginViewModel extends ViewModel {
             let data: LoginModel = params.Model !== undefined ? params?.Model[0] : {}
             data.InstitutionCode = '202062418221';
             let request = new LoginRequest(data);
+            console.log(JSON.stringify(request))
             let res = await this.Manager.PostData(request);
             response = new LoginResponse(res);
             if (!ViewModel.IsNullOrUndefined(response)) {
-                // switch (response.Code) {
-                //     case '00': {
-                //         context.actions.setAuthDetails(response.UserModel.ID?.toString()?? '', response.RedirectParams);
-                //         const test = context.actions.getAuthDetails();
-                //         response.Message = 'Welcome';
-                //         response.Redirect = true;
-                //         response.RedirectPath = '/dashboard';
-                //         break;
-                //     }
-                //     case 'AE': {
-                //         response.Message = 'Invalid Login Credentials';
-                //         break;
-                //     }
-                //     default: {
-                //         response.Message = 'Something terrible happened';
-                //         break;
-                //     }
-                // }
+                switch (response.Code) {
+                    case '00': {
+                        context.actions.setAuthDetails(response.UserModel.ID?.toString()?? '', response.RedirectParams);
+                        const test = context.actions.getAuthDetails();
+                        response.Message = 'Welcome';
+                        response.Redirect = true;
+                        response.RedirectPath = '/dashboard';
+                        break;
+                    }
+                    case 'AE': {
+                        response.Message = 'Invalid Login Credentials';
+                        break;
+                    }
+                    default: {
+                        response.Message = 'Something terrible happened';
+                        break;
+                    }
+                }
 
                 return response;
             } else {
