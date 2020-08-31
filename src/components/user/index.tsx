cdf4cbdb6085712656715db5858fa1cdd9b62bb1
Joorgi Spectra <sazeespectra@gmail.com>
2020-08-31 11:51:24 -0400

    google signin -bug

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,6 +1,4 @@
 import Login from './login/login';
 import SignUp from './signup';
-import LoginByGoogle from './google/loginByGoogle'
 export default Login;
 export {SignUp};
-export {LoginByGoogle};
