da82785258c22be32378fc25ed1a25459c1cab96
Joorgi Spectra <sazeespectra@gmail.com>
2020-08-31 17:31:51 -0400

    exchange -bug

--- src/components/bills/exchange/index.tsx
+++ src/components/bills/exchange/index.tsx
@@ -28,7 +28,7 @@ export interface SearchParametersProps {
     direction: Direction;
     SetCount: (e: number) => void;
     ToString(): Promise<string> | null;
-    ToObject(value:string):Promise<IHttpObject> | null;
+    ToObject(value: string): Promise<IHttpObject> | null;
 }
 
 const Exchange: React.FC = () => {
@@ -43,9 +43,9 @@ const Exchange: React.FC = () => {
             sort: 'Amount',
             direction: Direction.asc,
             searchBarItem: '',
-            SetCount: (e: number)=>{},
-            ToString: ()=> {return null},
-            ToObject: (value: string)=> {return null}
+            SetCount: (e: number) => { },
+            ToString: () => { return null },
+            ToObject: (value: string) => { return null }
         }
     )
     const [records, setRecords] = useState<SocialTableRowProps[]>();
@@ -53,40 +53,44 @@ const Exchange: React.FC = () => {
     const [showDetailsModal, setShowDetailsModal] = useState(false);
     const [exchangeModel, setExchangeModel] = useState<ExchangeModel>();
     const [showLoader, setShowLoader] = useState(false);
-    
+
     const viewExchange = useCallback((record?: ExchangeModel) => {
         setShowDetailsModal(true);
         setExchangeModel(record);
         //get details from state and send to Modal
     }, [])
-    const setCount = useCallback((count: number)=>{
+    const setCount = useCallback((count: number) => {
         let x = searchParameters;
-            if (x !== undefined) {
-                x.count = count;
-            }
-            setSearchParameters(x);
+        if (x !== undefined) {
+            x.count = count;
+        }
+        setSearchParameters(x);
     }, [])
     const process = useCallback(
         async () => {
             setShowLoader(true)
             let recordList: SocialTableRowProps[] = [];
-                let searchParams = new AllExchangesRequest(searchParameters);
-                const viewModel = new AllExchangesViewModel(searchParams)
-                let r = await viewModel.SubmitAction(searchParams);
-                //const {model , count} = r.Model
-                setCount(r.Model !== undefined ? r.count : 0)
-                const model = r.Model;
-                model?.forEach(element => {
-                    let exchange: ExchangeModel = new ExchangeModel(element);
-                    let row: SocialTableRowProps = {
-                        viewAction: () => { viewExchange(exchange) },
-                        id: exchange.ID,
-                        record: exchange
-                    }
-                    recordList.push(row);
-                })
-                setRecords(recordList)
-                setShowLoader(false);
+            let auth = context.actions.getAuthDetails();
+            let criteria = { fieldName: 'RequesterID', fieldValue: auth.Model.UserModel.ID };
+            let search = searchParameters;
+            search.Criteria.push(criteria);
+            let searchParams = new AllExchangesRequest(search);
+            const viewModel = new AllExchangesViewModel(searchParams)
+            let r = await viewModel.SubmitAction(searchParams);
+            //const {model , count} = r.Model
+            setCount(r.Model !== undefined ? r.count : 0)
+            const model = r.Model;
+            model?.forEach(element => {
+                let exchange: ExchangeModel = new ExchangeModel(element);
+                let row: SocialTableRowProps = {
+                    viewAction: () => { viewExchange(exchange) },
+                    id: exchange.ID,
+                    record: exchange
+                }
+                recordList.push(row);
+            })
+            setRecords(recordList)
+            setShowLoader(false);
         },
         [searchParameters, setCount, viewExchange],
     )
@@ -97,7 +101,7 @@ const Exchange: React.FC = () => {
     const Submit = async () => {
         await process()
     }
-    const SetSearchCriteria = async(e: any) => {
+    const SetSearchCriteria = async (e: any) => {
         let x: SearchParametersProps = searchParameters;
         let y: ISearchCriteria = {
             fieldName: "Amount",
@@ -113,9 +117,9 @@ const Exchange: React.FC = () => {
         let name: string = e.target.name;
         (x as any)[name] = e.target.value;
         setSearchParameters(x);
-        if(e.target.name !== 'searchBarItem'){
+        if (e.target.name !== 'searchBarItem') {
             await Submit();
-        }else{
+        } else {
             SetSearchCriteria(e);
         }
     }
@@ -126,7 +130,7 @@ const Exchange: React.FC = () => {
     function toggleDetailsModal(e: any) {
         setShowDetailsModal(!showDetailsModal)
     }
-    
+
     return (<>
         <Content>
             <ExchangeToolBar
@@ -135,10 +139,10 @@ const Exchange: React.FC = () => {
                 setSearchParam={SetSearchParameters}
                 showModal={(e: any) => setShowModal(true)} />
             <AllExchanges
-            toggleModal={toggleDetailsModal}
-            exchange={exchangeModel}
-            showDetails={showDetailsModal} 
-            records={records? records : []} />
+                toggleModal={toggleDetailsModal}
+                exchange={exchangeModel}
+                showDetails={showDetailsModal}
+                records={records ? records : []} />
             <Pager
                 direction={searchParameters ? searchParameters.direction : Direction.asc}
                 setDirection={SetSearchParameters}
@@ -168,7 +172,7 @@ const Exchange: React.FC = () => {
                 </ModalBody>
             </StyledModal>
         </Content>
-    <ActivityIndicator display={showLoader} show={showLoader} title={'Loading Xchanges'}/>
+        <ActivityIndicator display={showLoader} show={showLoader} title={'Loading Xchanges'} />
     </>)
 }
 export default Exchange;
