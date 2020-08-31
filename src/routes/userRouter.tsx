16915ad6b531b6c6fc64a9f35a38a9af1a9e1f91
Joorgi Spectra <sazeespectra@gmail.com>
2020-08-31 12:04:47 -0400

    google signin -bug

--- src/routes/userRouter.tsx
+++ src/routes/userRouter.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { SignUp, LoginByGoogle } from '../components/user';
+import { SignUp } from '../components/user';
 
 
 const UserRouter = () => (
     <Switch>
         <Route exact path='/user' component={SignUp}/>
-        <Route path='/user/google' component={LoginByGoogle}/>
     </Switch>
 )
 export default UserRouter
